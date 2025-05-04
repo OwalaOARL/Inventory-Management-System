9876f0595da3dc9bd0a41ec1663e6d771962b7d8
OwalaOARL <112147607+OwalaOARL@users.noreply.github.com>
2025-05-04 21:53:17 +0530

    add loginpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from './Components/Navbar';
+import Loginpage from "./Components/Loginpage";
 
 const App = () => {
   return (
     <div> 
-      <Navbar/>
+      <Loginpage/>
     </div>
   );
 }
